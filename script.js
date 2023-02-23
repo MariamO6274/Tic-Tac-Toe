313cc928deb6d1326cd15023eb1e4630c44d14fe
MariamO6274 <ostatishvili@gmail.com>
2023-02-22 18:50:05 -0700

    click functions JS
    JS, events, click functions, class list object

---
+++ script.js
@@ -0,0 +1,26 @@
+const choiceButtons = document.querySelectorAll(".btn-box");
+const home = document.querySelector("#home");
+const board = document.querySelector("#board");
+
+let player1 = "x";
+let mode = "cpu";
+
+const activateChoice = (icon) => {
+  if (icon === "x") {
+    choiceButtons[0].classList.add("active");
+    choiceButtons[1].classList.remove("active");
+    let player1 = "x";
+  } else {
+    choiceButtons[1].classList.add("active");
+    choiceButtons[0].classList.remove("active");
+    let player1 = "o";
+  }
+  console.log(player1);
+};
+
+const startGame = (modeParam) => {
+  home.style.display = "none";
+  board.style.display = "flex";
+  mode = modeParam;
+  console.log(mode);
+};
